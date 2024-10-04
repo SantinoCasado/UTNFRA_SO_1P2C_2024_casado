d3396fb4fa13d975751f43a28b8a934700ad4405
Santino Casado <santinocasado@gmail.com>
2024-10-04 18:29:31 +0000

    Pequeño arreglo en puntoA

--- RTA_SCRIPT_Examen_20241004/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_A.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "==== Creo la estructura de directorio ===="
-mkdir -p $HOME/Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+mkdir -p $HOME/Examenes-UTN/{Alumno_{1..3}/parcial_{1..3},profesores}
 
 
 echo "==== Muestro la estructura ===="
