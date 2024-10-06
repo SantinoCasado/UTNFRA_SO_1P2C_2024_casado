ab6270026feb41189a2813c632581ab5845b28c3
Santino Casado <santinocasado@gmail.com>
2024-10-06 17:38:54 +0000

    script del punto E

---
+++ RTA_SCRIPT_Examen_20241004/Punto_E.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Obtener total de memoria RAM
+total_ram=$(grep MemTotal /proc/meminfo | awk '{print $2, $3}')
+
+# Obtengo el fabricante del chasis
+chassis=$(sudo dmidecode -s chassis-manufacturer)
+
+
+echo MemTotal: $total_ram >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
+free -h |awk '{print $7}' >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
+
+echo Chasis Information >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
+	echo Manufacturer: $chassis >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
+	free -h |awk '{print $7}' >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
+
