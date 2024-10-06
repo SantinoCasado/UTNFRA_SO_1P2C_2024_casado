2298cbb36f6ffde1670fc8f18481e193e4d43063
Santino Casado <santinocasado@gmail.com>
2024-10-06 17:43:32 +0000

    Pequeños cambios en el punto E

--- RTA_SCRIPT_Examen_20241004/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_E.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
 # Obtener total de memoria RAM
 total_ram=$(grep MemTotal /proc/meminfo | awk '{print $2, $3}')
 
 # Obtengo el fabricante del chasis
 chassis=$(sudo dmidecode -s chassis-manufacturer)
-
 
 echo MemTotal: $total_ram >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
 free -h |awk '{print $7}' >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
@@ -14,4 +12,3 @@ free -h |awk '{print $7}' >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_E
 echo Chasis Information >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
 	echo Manufacturer: $chassis >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
 	free -h |awk '{print $7}' >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
-
