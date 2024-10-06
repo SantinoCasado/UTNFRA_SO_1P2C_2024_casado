b6dc8e2751dda00b3f9c64247e87b532bce9d205
Santino Casado <santinocasado@gmail.com>
2024-10-06 17:47:29 +0000

    Pequeño cambio punto E

--- RTA_SCRIPT_Examen_20241004/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_E.sh
@@ -7,8 +7,6 @@ total_ram=$(grep MemTotal /proc/meminfo | awk '{print $2, $3}')
 chassis=$(sudo dmidecode -s chassis-manufacturer)
 
 echo MemTotal: $total_ram >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
-free -h |awk '{print $7}' >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
 
 echo Chasis Information >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
-	echo Manufacturer: $chassis >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
-	free -h |awk '{print $7}' >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
+echo 		Manufacturer: $chassis >> ~/repogit/UTNFRA_SO_1P2C_2024_casado/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
