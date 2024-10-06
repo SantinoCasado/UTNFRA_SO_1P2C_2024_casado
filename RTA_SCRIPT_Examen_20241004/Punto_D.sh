74f2360c1f0f01268e706dc50096911f167313a6
Santino Casado <santinocasado@gmail.com>
2024-10-06 16:52:42 +0000

    Modifico el script del Punto_D

--- RTA_SCRIPT_Examen_20241004/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_D.sh
@@ -1,5 +1,6 @@
-#Creo los directorios con sus respectivas ramas
-mkdir -p $HOME/Estructura_Asimetrica/{correo,clientes} && touch $HOME/Estructura_Asimetrica/correo/cartas_{1..100},$HOME/Estructura_Asimetrica/correo/carteros_{1..10},$HOME/Estructura_Asimetrica/clientes/cartas_{1..100}
+#! /bin/bash
+#Creo la estructura de directorios
+mkdir -p $HOME/Estructura_Asimetrica/{correo/cartas_{1..100},clientes/cartas_{1..100}} && mkdir -p $HOME/Estructura_Asimetrica/correo/carteros_{1..10}
 
 echo "Muestro la estructura de los directorios"
-tree $HOME/Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
+tree $HOME/Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 90 --column 4
