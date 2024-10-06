91c0af5b6c894a83ff198e3265a14e1fdbc6a868
Santino Casado <santinocasado@gmail.com>
2024-10-06 19:00:19 +0000

    Pequeña modificacion en el punto b

--- RTA_SCRIPT_Examen_20241004/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_B.sh
@@ -35,3 +35,14 @@ for i in {5..14}; do
 done
 echo "==== Formateo completados. ===="
 
+echo "==== Monto las pariciones ===="
+echo "${DISCO}5 $HOME/Examenes-UTN/Alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}6 $HOME/Examenes-UTN/Alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}7 $HOME/Examenes-UTN/Alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}8 $HOME/Examenes-UTN/Alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}9 $HOME/Examenes-UTN/Alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}10 $HOME/Examenes-UTN/Alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}11 $HOME/Examenes-UTN/Alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}12 $HOME/Examenes-UTN/Alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}13 $HOME/Examenes-UTN/Alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}14 $HOME/Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
